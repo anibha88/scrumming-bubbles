04c7b92975dec1b8b722074072b28ad4bb827e78
anibha88 <anibha88@gmail.com>
2016-05-31 18:10:42 +0530

    Fix for invalid authenticity token

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  #skip_before_filter :verify_authenticity_token, :only => [:create]
+  skip_before_filter :verify_authenticity_token, :only => [:create]
 
 
   # GET /listings
